d156ecfd28c20c848f22c20c67dde686c88e08ec
Rody Davis <rody.davis.jr@gmail.com>
2021-06-03 18:25:54 -0700

    updating build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lit-html-table/",
+  base: '/lit-html-table/',
   build: {
     lib: {
       entry: "src/lit-html-table.ts",
       formats: ["es"],
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
     },
   },
 });
